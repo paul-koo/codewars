2a908a132d8fbceb33fd72d0ea9a39b19ba075ba
paul-koo <pavelkoltun999@gmail.com>
2024-05-02 21:58:42 +0300

    feat: 8kyu Reversed Strings

---
+++ Difficulty_8_kyu/Reversed Strings.js
@@ -0,0 +1,11 @@
+// Reversed Strings
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str){
+  return str.split('').reverse().join('');
+}
+
+console.log(solution('world'));
