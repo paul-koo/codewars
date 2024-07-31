ee5ae2f7255234aa000975f5f8d5e98064c2be3a
paul-koo <pavelkoltun999@gmail.com>
2024-07-31 14:50:37 +0300

    feat: 8kyu Even or Odd

---
+++ Difficulty_8_kyu/Even or Odd.js
@@ -0,0 +1,9 @@
+//Even or Odd
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(number) {
+    return number % 2 === 0 ? "Even" : "Odd";
+}
+
+console.log(evenOrOdd(2)); //"Even"
+console.log(evenOrOdd(7)); //"Odd"
