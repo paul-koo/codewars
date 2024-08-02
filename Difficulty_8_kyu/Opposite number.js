ac05b0c5a29e5a39dd54a380740f95fff928bfcc
paul-koo <pavelkoltun999@gmail.com>
2024-08-02 23:58:45 +0300

    feat: 8kyu Opposite number

---
+++ Difficulty_8_kyu/Opposite number.js
@@ -0,0 +1,15 @@
+//Opposite number
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+// Examples:
+// 1: -1
+// 14: -14
+// -34: 34
+
+function opposite(number) {
+    return number * -1;
+}
+
+console.log(opposite(1)); //-1
+console.log(opposite(33)); //-33
+console.log(opposite(-59)); //59
