147da82d331c3b2f84a0142d69f5706f9d298904
paul-koo <pavelkoltun999@gmail.com>
2024-08-02 23:48:22 +0300

    feat: 8kyu Largest Square Inside A Circle

---
+++ Difficulty_8_kyu/Largest Square Inside A Circle.js
@@ -0,0 +1,10 @@
+//Largest Square Inside A Circle
+// Determine the area of the largest square that can fit inside a circle with radius r.
+
+function areaLargestSquare(r) {
+    return 2 * r ** 2;
+}
+
+console.log(areaLargestSquare(5)); //50
+console.log(areaLargestSquare(7)); //98
+console.log(areaLargestSquare(15)); //450
