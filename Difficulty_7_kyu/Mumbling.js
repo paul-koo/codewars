3e2eedd268b9a983fa56887f31cd3e86c050331f
paul-koo <pavelkoltun999@gmail.com>
2024-09-29 15:46:46 +0300

    feat: 7kyu Mumbling

---
+++ Difficulty_7_kyu/Mumbling.js
@@ -0,0 +1,22 @@
+//Mumbling
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+function accum(s) {
+    return s
+        .split('')
+        .map(
+            (el, index) =>
+                el.toUpperCase() + Array(index).fill(el.toLowerCase()).join('')
+        )
+        .join('-');
+}
+
+console.log(accum('ZpglnRxqenU')); //"Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+console.log(accum('NyffsGeyylB')); //"N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
+console.log(accum('MjtkuBovqrU')); //"M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
